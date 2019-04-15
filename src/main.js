a400481dfbb31117a101cd9c5c25ea519f0fa9cc
camilo alaniz <calaniz@option.cl>
2019-04-15 10:56:33 -0400

    added .env

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+import firebase from "firebase";
+
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+};
+firebase.initializeApp(config);
+
 Vue.config.productionTip = false;
 
 new Vue({
